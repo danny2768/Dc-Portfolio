e5ab038432522da720df14a73e470993a96511ee
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-04-01 01:01:02 -0500

    31/03/2024: resumePage

--- src/app/portfolio/services/portfolio.service.ts
+++ src/app/portfolio/services/portfolio.service.ts
@@ -7,9 +7,9 @@ import { MenuItems } from '../interfaces/menu-item.interface';
 export class PortfolioService {
 
   private menuItems: MenuItems[] = [
-    {title: 'Home', url: 'home'},
-    {title: 'Projects', url: 'projects'},
-    {title: 'Resume', url: 'resume'},
+    {title: 'Home', url: '/portfolio/home'},
+    {title: 'Projects', url: '/portfolio/projects'},
+    {title: 'Resume', url: '/portfolio/resume'},
   ];
 
   constructor() { }
