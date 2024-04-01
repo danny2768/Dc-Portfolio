2fd4dfc1e08ff3d6acea173ab20d3eaf40872b0e
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-03-31 20:55:21 -0500

    31/03/2024: navbar implementation

--- src/app/portfolio/components/nav-bar/nav-bar.component.ts
+++ src/app/portfolio/components/nav-bar/nav-bar.component.ts
@@ -1,33 +1,69 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import gsap from 'gsap';
-
-interface MenuItems {
-  title: string;
-  url: string;
-}
+import { MenuItems } from '../../interfaces/menu-item.interface';
+import { PortfolioService } from '../../services/portfolio.service';
 
 @Component({
   selector: 'portfolio-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent{
+export class NavBarComponent {
 
   public showMobileMenu: boolean = false;
+  public menuItems: MenuItems[];
+  private navHidden: boolean = false;
+  private lastScrollPosition: number = 0;
 
-  public menuItems: MenuItems[] = [
-    {title: 'Home', url: 'home'},
-    {title: 'Projects', url: 'projects'},
-    {title: 'Resume', url: 'resume'},
-  ];
-
-  // GSAP annimations
-
-
-  toggleMobileMenu( annimation?: boolean){
-    this.showMobileMenu = !this.showMobileMenu;
-
+  constructor(private portfolioService: PortfolioService) {
+    this.menuItems = this.portfolioService.getMenuItems();
   }
 
-}
+  ngAfterViewInit() {
+    this.updateNavbarColor();
+  }
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+    const currentScrollPosition = window.pageYOffset;
+    if (currentScrollPosition < this.lastScrollPosition) {
+      this.navHidden = false; // Scrolling up
+    } else {
+      this.navHidden = true; // Scrolling down
+      this.showMobileMenu = false; // Hide mobile menu when scrolling down
+    }
+    this.lastScrollPosition = currentScrollPosition;
+
+    gsap.to('#navbar', { duration: 0.5, y: this.navHidden ? -100 : 0 });
+
+    this.updateNavbarColor();
+  }
+
+  toggleMobileMenu() {
+    this.showMobileMenu = !this.showMobileMenu;
+    this.updateNavbarColor();
+  }
+
+  private updateNavbarColor() {
+    const navbarElement = document.getElementById('navbar');
+    if (!navbarElement) return;
+
+    if (this.showMobileMenu) {
+      gsap.to(navbarElement, {
+        backgroundColor: 'rgba(50, 50, 50, 0.8)',
+        color: 'white',
+        duration: 0.5,
+      });
+    } else {
+      const navbarHeight = navbarElement.offsetHeight + 10;
+      const currentScrollPosition = window.pageYOffset;
+
+      gsap.to(navbarElement, {
+        backgroundColor:
+          currentScrollPosition > navbarHeight ? 'rgba(50, 50, 50, 0.8)' : 'transparent',
+        color: currentScrollPosition > navbarHeight ? 'white' : 'black',
+        duration: 0.5,
+      });
+    }
+  }
+}
